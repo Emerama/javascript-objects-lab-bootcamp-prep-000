62cca6407070993fc4c0bdae00501803807a55b8
Emily Ranson <ransonme@tcd.ie>
2018-01-22 15:25:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  const newObj = Object.assis({}, object)
-  delete newObj[key]
-  return newObj
+  const newObj = Object.assis({}, object);
+  delete newObj[key];
+  return newObj;
 }
